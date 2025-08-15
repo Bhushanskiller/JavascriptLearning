95a7b16999f76df446c9d50961d346f39c108356
Bhushan Ganesh Shelar <65867993+Bhushanskiller@users.noreply.github.com>
2025-08-15 07:47:44 +0000

    ECMA sites added

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,19 +1,15 @@
 "use strict"; // treat all JS code as newer version
-
 // alert( 3 + 3) // we are using nodejs, not browser
-
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+//https://tc39.es/ecma262/
 console.log(3 
     +
      3) // code readability should be high don't do this way 
-
 console.log("Bhushan")
-
-
 let name = "Bhushan"
 let age = 18
 let isLoggedIn = false
 let state;
-
 // number => 2 to power 53
 // bigint
 // string => ""
@@ -22,8 +18,6 @@ let state;
 // undefined => 
 // symbol => unique
 
-
 // object
-
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
