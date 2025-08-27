4edd17daaebd4d5d6145c59ae882bc1826e774d6
Bhushan Ganesh Shelar <65867993+Bhushanskiller@users.noreply.github.com>
2025-08-27 15:40:40 +0000

    String issue with numbers and postfix prefix increment and decrement operation examples

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -92,13 +92,171 @@ let stringNumber = String(someNumber)
 
 let someNull = null
 let stringNull = String(someNull)
-console.log(stringNull)// null 
-console.log(typeof stringNull)//string
+//console.log(stringNull)// null 
+//console.log(typeof stringNull)//string
 
 let someUndefined = undefined
 let stringUndefined = String(someUndefined)
-console.log(stringUndefined)// undefined 
-console.log(typeof stringUndefined)//string
+//console.log(stringUndefined)// undefined 
+//console.log(typeof stringUndefined)//string
+
+// String to number confusion
+// console.log(2+3);//5
+// console.log(2*3);//6
+/*In JavaScript, the operator ** is the exponentiation operator.
+It raises the number on the left to the power of the number on the right. 
+take 2 raised to the power 5
+2^5=2×2×2*2*2=8
+*/
+// console.log(2**5);//32
+// console.log(-8);//-8
+
+//let str1, str2,str3
+str1 = "Bhushan"
+str2 = " Shelar" 
+str3 = str1+str2
+// console.log("stringCheck => "+str3) //Bhushan Shelar
+str1 = null
+str2 = null
+str3 = str1+str2
+// console.log("nullCheck => "+str3) //0
+// console.log("nullChecktypeOf => "+typeof str3) //0
+
+let strUndefinedTypeCheck;
+// console.log("undefinedtypeOf strUndefinedTypeCheck=> "+typeof strUndefinedTypeCheck) //undefined
+str1 = undefined
+str2 = undefined
+str3 = str1+str2
+// console.log("undefinedCheck => "+str3)//NaN
+// console.log("undefinedtypeOf str3=> "+typeof str3) //number
+
+
+//String Number Concatenation Scenarios
+/* 
+Document for Primitive Type Data Conversion
+URL : https://tc39.es/ecma262/#sec-type-conversion 
+*/
+
+/* If string is found in start for eg. "1"+2+3 then it will considered as a complete string and output will be 123*/
+// console.log("1"+2+3);//123
+
+// console.log(1+"2"+3);//123
+// console.log(1+2+3+"4"+5+6);//6456 Here it starts with Numeric values and from "4" it will start considering it as string
+// console.log("4"+"5"); //45
+
+//Increment and Decrement operation Scenarios
+// console.log(true);//true
+// console.log(+true);//1 
+// console.log(false);//false
+// console.log(+false);//0
+//Wrong Implementation will give compile time error
+/* console.log(++true);//1 //Invalid left-hand side expression in prefix operation
+console.log(true+); //error SyntaxError: Unexpected token ')' */
+
+// console.log("string=> "+"");
+//Here we are just passing +"" in console.log
+//stringWithIncrementPrefix
+// console.log(+"");
+//console.log(++""); //Invalid left-hand side expression in prefix operation
+
+// console.log("null=> "+null);
+//Here we are just passing +null in console.log
+//nullWithIncrementPrefix
+// console.log(+null);
+
+// console.log("undefined=> "+undefined);
+//Here we are just passing +undefined in console.log
+// undefinedWithIncrementPrefix
+// console.log(+undefined);
+
+//Prefix Increment and Decrement
+// console.log("Increment")
+// let prefixGameCounter = 1001;
+// console.log(+prefixGameCounter); //1001
+// console.log(++prefixGameCounter); //1002
+// //console.log(+++prefixGameCounter); //Invalid left-hand side expression in prefix operation
+// console.log(++prefixGameCounter); //1003
+// console.log("Decrement")
+// console.log(-prefixGameCounter); //1001
+// console.log(--prefixGameCounter); //1002
+// //console.log(+++prefixGameCounter); //Invalid left-hand side expression in prefix operation
+// console.log(--prefixGameCounter); //1003
+
+
+//Postfix Increment and Decrement
+// console.log("Increment")
+// let postfixGameCounter = 1001;
+//console.log(postfixGameCounter+); //Unexpected token ')'
+// console.log(postfixGameCounter++); //1001 //line A
+//console.log(postfixGameCounter+++); //Unexpected token ')'
+// console.log(postfixGameCounter++); //1002 //line B
+// console.log("Decrement")
+//console.log(postfixGameCounter-); //Unexpected token ')'
+// console.log(postfixGameCounter--); //1003 //line C
+//console.log(postfixGameCounter+++); //Unexpected token ')'
+// console.log(postfixGameCounter--); //1002 //line D
+
+/*
+Step-by-step execution
+console.log(postfixGameCounter++);//line A 1001
+console.log(postfixGameCounter++);//line B 1002
+console.log(postfixGameCounter--);//line C 1003
+console.log(postfixGameCounter--);//line D 1002
+Start:
+postfixGameCounter = 1001 
+ 
+--> line A console.log(postfixGameCounter++);
+++ is postfix, so:
+First return the current value (1001)
+Then increment it (1002)
+Output: 1001
+Now postfixGameCounter = 1002
+
+Line B console.log(postfixGameCounter++);
+Current value: 1002
+Postfix means print first, increment after
+Output: 1002
+Now postfixGameCounter = 1003
+
+Line C console.log(postfixGameCounter--);
+Current value: 1003
+Postfix -- means print first, decrement after
+Output: 1003 <- This is why you see 1003
+Now postfixGameCounter = 1002
+
+Line D  console.log(postfixGameCounter--);
+Current value: 1002
+Postfix: print first, decrement after
+Output: 1002
+Now postfixGameCounter = 1001
+*/
+
+
+/* 
+URL : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+Key Rule
+x++ → return x, then do x = x + 1
+x-- → return x, then do x = x - 1 
+*/
+
+let x =3
+console.log(x) //3
+
+/* 
+Here output will be 3 as it will print output first and then do postfix operation 
+(In stack it updated to 4 after postfix operation) 
+*/
+console.log(x++) //3 
+
+console.log(x)//4
+
+
+
+
+
+
+
+
 
 
 
